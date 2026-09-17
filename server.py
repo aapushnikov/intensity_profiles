0446429dff77652595b0c52023b4add74dc29efc
aapushnikov <andrej.pushkinov@yandex.ru>
2026-09-17 22:15:56 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,24 @@
 from fastapi import FastAPI, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 import uuid
 import os
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 os.makedirs("dataset", exist_ok=True)
+
 
 @app.get("/")
 def home():
     return {"status": "ok"}
+
 
 @app.post("/save")
 async def save(file: UploadFile):
